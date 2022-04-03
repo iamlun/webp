3311159d9ba5b2e7cd3d50a7d88fe30551c47c2d
iamlun <100269372+iamlun@users.noreply.github.com>
2022-04-03 14:45:37 +0800

    add qz3

---
+++ qz3/quiz3.js
@@ -0,0 +1,6 @@
+
+var input=document.getElementById("input");
+input.addEventListener("keydown",logkey);
+function logkey(e){
+    word.textContent = `${e.key}`;//event.key
+}//if ``中 有包變數 則會輸出該變數的內容
